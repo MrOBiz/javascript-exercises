ff07fc3e162f0fc58dbea545497c09ab57dc003b
Omar Biz <189382177+MrOBiz@users.noreply.github.com>
2026-07-02 12:37:00 +0200

    Solved JS foundations ex.08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    
+    return array.filter(x => !args.includes(x));
+
+    //ONLY ONE VAL
+    /*for(let i =0; i < array.length; i++){
+        if(array[i] == val){
+            array.splice(i, 1);
+            i--;
+        }
+    }*/
+
+    //return array;
 };
 
 // Do not edit below this line
