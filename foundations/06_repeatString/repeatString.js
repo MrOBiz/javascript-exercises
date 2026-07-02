44069eb12aac7c617542f9da50ce03a78278e09b
Omar Biz <189382177+MrOBiz@users.noreply.github.com>
2026-07-02 09:15:29 +0200

    Solved JS foundations ex.06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num < 0){
+        return "ERROR";
+    }else{
+        let str = "";
+        while(num > 0){
+            str = str + string;
+            num--;
+        }
+        return str;
+    }
+    
 };
 
 // Do not edit below this line
