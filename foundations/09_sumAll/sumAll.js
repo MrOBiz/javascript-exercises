a507100ecc9815a9ce4073c562a179930246787c
Omar Biz <189382177+MrOBiz@users.noreply.github.com>
2026-07-02 12:57:35 +0200

    Solved JS foundations ex.09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
+    if (a < 0 || !Number.isInteger(a)){
+        return "ERROR";
+    }else if(b < 0 || !Number.isInteger(b)){
+        return "ERROR";
+    }else{
+        let c = a - b;
 
+        if(c > 0){
+            for(let i = b; i <= a; i++){
+                sum += i;
+            }
+        }else if(c < 0){
+            for(let i = a; i <= b; i++){
+                sum += i;
+            }
+        }else if(c = 0){
+            sum = a;
+        }
+        
+        return sum;
+    }
 };
 
 // Do not edit below this line
